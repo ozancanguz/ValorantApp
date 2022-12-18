57a22eef1f3103eb19abcd1c7dcdf0fea418ef3c
Ozan <ozancangu24@gmail.com>
2022-12-18 22:23:35 +0300

    -data displayed on logcat

--- app/src/main/java/com/ozancanguz/valorantapp/ui/fragments/agentslist/AgentList.kt
+++ app/src/main/java/com/ozancanguz/valorantapp/ui/fragments/agentslist/AgentList.kt
@@ -1,11 +1,13 @@
 package com.ozancanguz.valorantapp.ui.fragments.agentslist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.ozancanguz.valorantapp.R
 import com.ozancanguz.valorantapp.databinding.FragmentAgentDetailsBinding
 import com.ozancanguz.valorantapp.databinding.FragmentAgentListBinding
@@ -28,10 +30,18 @@ class AgentList : Fragment() {
         _binding = FragmentAgentListBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        mainviewmodel.requestAgents()
+       observeLiveData()
+
 
 
         return view
+    }
+
+    private fun observeLiveData() {
+        mainviewmodel.requestAgents()
+        mainviewmodel.agentsList.observe(viewLifecycleOwner, Observer {
+            Log.d("agentlist", "" +it)
+        })
     }
 
 
